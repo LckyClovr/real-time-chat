52a45f17ea543572ebf8e28e3b97bf2cd7b2f31b
Anthony Simmons <lckyclovrstudio@gmail.com>
2025-03-26 13:58:34 -0500

    Replace TypeScript configuration with JavaScript and update Next.js settings for images, webpack, TypeScript, and ESLint

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
