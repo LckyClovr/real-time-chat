587729827f3aa9d94bd7c7535bdcb7b148c42193
Michael Baxter <mjb210008@utdallas.edu>
2025-03-28 10:39:45 -0500

    Basic background and scaling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
+"use client";
 import Image from "next/image";
+import React from 'react';
+import { useState } from 'react';
 
-export default function Home() {
+
+const mainBackgroundColor = '#262626';
+const friendsBackgroundColor = '#202020';
+const chatroomSelectionBackgroundColor = '#191919';
+const textBoxColor = '#323232';
+//there are some weird numbers in the positioning and sizing that are taken by positioning manually using 
+//pixels then converting to a dynamic percentage of the viewport size so they scale with the window
+const Home: React.FC = () => {
+  console.log(window.innerHeight)// 1440 by 778 for some reason?
+  const [text, setText] = useState('');
+  const keyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(text) // right now it just outputs whatever you type into the console
+      // make a function that takes the text in the textbox as input
+      setText('') // remove the text in the chat box after hitting enter so we can type our next message
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value); // Update the state as the user types
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <div 
+          style={{ // rectangle for background of chatroom selection
+            width: '6.94vw',          // Width of the rectangle
+            height: '100vh',         // Height of the rectangle
+            backgroundColor: chatroomSelectionBackgroundColor,  // Background color of the rectangle
+          }}
+        />
+      <div 
+          style={{ // rectangle for background of friends list
+            width: '20.83vw',          // Width of the rectangle
+            height: '100vh',         // Height of the rectangle
+            backgroundColor: friendsBackgroundColor,  // Background color of the rectangle
+            position: 'absolute',
+            top: '0px',
+            left: '6.94vw'
+          }}
+        />
+        <div 
+          style={{ // rectangle for background of info on the users
+            width: '20.83vw',          // Width of the rectangle
+            height: '100vh',         // Height of the rectangle
+            backgroundColor: friendsBackgroundColor,  // Background color of the rectangle
+            position: 'absolute',
+            top: '0px',
+            left: '83.3vw'
+          }}
+        />
+      <div 
+          style={{ // rectangle for background of chat
+            width: '55.6vw',          // Width of the rectangle
+            height: '100vh',         // Height of the rectangle
+            backgroundColor: mainBackgroundColor,  // Background color of the rectangle
+            position: 'absolute',
+            top: '0px',
+            left: '27.78vw'
+          }}
+        />
+      <input 
+        type="text" 
+        id="chatTextBox" 
+        value = {text}
+        onChange={handleChange}
+        placeholder="Message @User" 
+        onKeyDown={keyDown}
+        style={{ 
+          width: '41.6vw', 
+          padding: '1.28vh' , 
+          paddingLeft: '4.16vw',
+          color: '#efefef', 
+          position: 'absolute',
+          top: '89.97vh', 
+          left: '34.72vw',
+          borderRadius: '1.28vh',
+          backgroundColor: textBoxColor,
+          outline: 'none',
+          fontSize: '1.11vw'
+        }} 
+        
+      />
+       
     </div>
   );
-}
+};
+
+export default Home;
